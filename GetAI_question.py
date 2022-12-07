a139bef911f1561e0d0f256871716c4ebf010de6
Zhong jia bin <www.9454@qq.com>
2022-12-08 02:33:59 +0800

    need to changes are Cookie ,X-CSRF-Token,data: in F12 : payload 'number' parameter. only get the json file!!!

--- GetAI_question.py
+++ GetAI_question.py
@@ -2,25 +2,25 @@ import re
 import requests
 
 url ='http://www.yaiedu.com/api/continue_answer'
+data = {'answer_record_id':'569015'}
 headers = {
             'Accept': 'application/vnd.edusoho.v2+json',
-            'Accept-Encoding': 'gzip, deflate',
+            'Accept-Encoding':'gzip,deflate',
             'Accept-Language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7',
-            'Cache-Control': 'no-cache',
             'Conetent-Length':'23',
             'Conetent-Type':'application/x-www-form-urlencoded; charset=UTF-8',
             'Proxy-Connection': 'keep-alive',
-            'Host': 'http://www.yaiedu.com',
+            'Host': 'www.yaiedu.com',
             'Origin': 'http://www.yaiedu.com',
-            'Cookie':'online-uuid=3852EE71-57EF-DD53-4FE0-36793C58D52A; PHPSESSID=a2afm17t3pf2cq555r3iger4c0; REMEMBERME=Qml6XFVzZXJcQ3VycmVudFVzZXI6ZFhObGNsOXBZemd4TnpKaVltMUFaV1IxYzI5b2J5NXVaWFE9OjE3MDE5MTU3NTE6MWE1YmE2OWVkNjNkNDRmYmRjOTBmMmQzODMxNDg0YjNhN2FlN2QxOTIyMzcxOTdhOGM3ZDUxOGQ1MGYxYTY5YQ==',
+            'Cookie':'online-uuid=7946ACD4-2B0C-E2C1-3713-B418A94951B0; is_skip_mobile_bind=0; PHPSESSID=smusch6ecoduve0uv17qn8a230; REMEMBERME=Qml6XFVzZXJcQ3VycmVudFVzZXI6ZFhObGNsOXBZemd4TnpKaVltMUFaV1IxYzI5b2J5NXVaWFE9OjE3MDE5NzIwMTQ6ZWU2NjllOWYyMDI3NjQ4NDI5ODMwOTc0YmI3ZjBjNDNiZThlNmU4YjE5MjBlODAxOWI1NDM1ODAyYTA5ZDhhZQ==',
             'Pragma':'no-cache',
             'Referer':'http://www.yaiedu.com/lesson/526/testpaper/51/do',
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36',
-            'X-CSRF-Token':'7HP1ixRDuNYFW5p8R6Fy4N0eR4BIXhVLJTezxUqlX14',
+            'X-CSRF-Token':'5bHd-jUvt--PXbJiho7NcggrdUO-ha0VDTQGP7xZDJM',
             'X-Requested-With':'XMLHttpRequest'
 }
 r = requests.Session()
-r = r.get(url=url,headers=headers,)
+response = r.post(data=data,url=url,headers=headers,)
 
-print(r.text)
+print(response.status_code)
 
