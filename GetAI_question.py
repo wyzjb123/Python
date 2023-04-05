903feff53d0b4b1112f25c0ad79409bafe5da857
zzzib <tyxzjb@gmail.com>
2023-04-05 13:03:09 +0800

    4.5 做到提取json目标文件

--- GetAI_question.py
+++ GetAI_question.py
@@ -1,8 +1,9 @@
 import re
+import json
 import requests
 
 url ='http://www.yaiedu.com/api/continue_answer'
-data = {'answer_record_id':'569015'}
+data = {'answer_record_id':'678823'}
 headers = {
             'Accept': 'application/vnd.edusoho.v2+json',
             'Accept-Encoding':'gzip,deflate',
@@ -12,15 +13,16 @@ headers = {
             'Proxy-Connection': 'keep-alive',
             'Host': 'www.yaiedu.com',
             'Origin': 'http://www.yaiedu.com',
-            'Cookie':'online-uuid=7946ACD4-2B0C-E2C1-3713-B418A94951B0; is_skip_mobile_bind=0; PHPSESSID=smusch6ecoduve0uv17qn8a230; REMEMBERME=Qml6XFVzZXJcQ3VycmVudFVzZXI6ZFhObGNsOXBZemd4TnpKaVltMUFaV1IxYzI5b2J5NXVaWFE9OjE3MDE5NzIwMTQ6ZWU2NjllOWYyMDI3NjQ4NDI5ODMwOTc0YmI3ZjBjNDNiZThlNmU4YjE5MjBlODAxOWI1NDM1ODAyYTA5ZDhhZQ==',
+            'Cookie':'online-uuid=4BD640D4-C217-9990-C38E-BF371B638D94; PHPSESSID=frnjc92jhal89qeucbep30v5m7; REMEMBERME=Qml6XFVzZXJcQ3VycmVudFVzZXI6ZFhObGNsOXBZemd4TnpKaVltMUFaV1IxYzI5b2J5NXVaWFE9OjE3MTIyMDEyNzM6ZmNjY2Q3MmVkN2UwNDdhMzdhNTNkYzZlOTY2NzY3NGUxZWY1YWFhMjRmMWUwMDJlZTY3MjY1MWZkN2VmYWFiZQ==',
             'Pragma':'no-cache',
-            'Referer':'http://www.yaiedu.com/lesson/526/testpaper/51/do',
+            'Referer':'http://www.yaiedu.com/lesson/1193/testpaper/185/do',
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36',
-            'X-CSRF-Token':'5bHd-jUvt--PXbJiho7NcggrdUO-ha0VDTQGP7xZDJM',
+            'X-CSRF-Token':'MZgBUYtyJoxSYSZpkZ-WXvVyQvcat0uu2HMenqq8ars',
             'X-Requested-With':'XMLHttpRequest'
 }
-r = requests.Session()
-response = r.post(data=data,url=url,headers=headers,)
+response = requests.post(data=data,url=url,headers=headers).json()
+temp = json.dumps(response,indent=4,ensure_ascii=False)
+# with open('test_data.json', 'w',encoding='utf-8') as json_file:
+#     json_file.write(temp)
 
-print(response.status_code)
-
+print(response['assessment']['sections'][0]['items'][0]['questions'][0]['stem'])
